f60bd05d4be78425c0302b300a9355e02635fd44
obetta1 <obettabenjamin44@gmail.com>
2023-10-27 15:52:37 +0100

    projet setup

---
+++ lib/fetures/home/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key, required String title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text('welcome to Benjamin portfolio ')],
+      ),
+    );
+  }
+}
